dbcdeff51d8fed7e071d8114d6e4fd3bdfe4358e
ved-thakre <vedantthakre7@gmail.com>
2023-07-02 21:24:06 +0530

    Day-29

--- L_119_Path_in_a_tree.cpp
+++ L_119_Path_in_a_tree.cpp
@@ -1,1 +1,17 @@
+#include <bits/stdc++.h> 
+bool fun(TreeNode<int>* root, vector<int>&ans, int x){
+	if(root == NULL) return false;
+	ans.push_back(root->data);
+	if(root->data == x) return true;
+	if(fun(root->left, ans, x) || fun(root->right, ans, x)) return true;
+	ans.pop_back();
+	return false;
 
+}
+vector<int> pathInATree(TreeNode<int> *root, int x)
+{
+    vector<int>ans;
+	fun(root, ans, x);
+	return ans;
+}
+ 
