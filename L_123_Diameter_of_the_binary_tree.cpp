6eb771507772eadbd2abe3e97da224dcf3767baf
ved-thakre <vedantthakre7@gmail.com>
2023-07-04 22:38:59 +0530

    Day-31

---
+++ L_123_Diameter_of_the_binary_tree.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+int height(TreeNode<int> *node, int & diameter)
+{
+    if(!node)
+    return  0;
+
+    int lh=height(node->left,diameter);
+    int rh=height(node->right, diameter);
+    diameter=max(diameter, lh+rh);
+    return 1+max(lh,rh);
+}
+int diameterOfBinaryTree(TreeNode<int> *root)
+{
+	// Write Your Code Here.
+    int diameter=0;
+    height(root, diameter);
+    return diameter;
+}
