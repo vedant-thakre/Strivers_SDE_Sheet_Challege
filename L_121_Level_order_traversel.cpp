cf74d4a415ade220c27f49a2d0562af44b8d5a31
ved-thakre <vedantthakre7@gmail.com>
2023-07-04 22:38:41 +0530

    Day-31

---
+++ L_121_Level_order_traversel.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h> 
+/************************************************************
+
+    Following is the BinaryTreeNode class structure
+
+    template <typename T>
+    class BinaryTreeNode {
+       public:
+        T val;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+
+        BinaryTreeNode(T val) {
+            this->val = val;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+************************************************************/
+vector<int> getLevelOrder(BinaryTreeNode<int> *root){
+        vector<int> ans;
+        if(root==NULL) return ans;
+        queue<BinaryTreeNode<int>*> q;
+        q.push(root);
+        while(!q.empty()){
+            int size=q.size();
+            for(int i=0; i<size; i++){
+                BinaryTreeNode<int>* node=q.front();
+                q.pop();
+                if(node->left!=NULL) q.push(node->left);
+                if(node->right!=NULL) q.push(node->right);
+                ans.push_back(node->val);
+            }
+
+        }
+        return ans;
+}
