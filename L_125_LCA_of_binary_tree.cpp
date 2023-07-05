07f945014273e723738bf77010c3286dabf2a437
ved-thakre <vedantthakre7@gmail.com>
2023-07-05 22:15:39 +0530

    Day-32

--- L_125_LCA_of_binary_tree.cpp
+++ L_125_LCA_of_binary_tree.cpp
@@ -1,1 +1,21 @@
+#include <bits/stdc++.h> 
+
+int rec(TreeNode<int> *root, int x, int y){
+
+    if(!root)
+    return -1;
+    if(root->data==x || root->data==y)
+    return root->data;
+    int l=rec(root->left,x,y);
+    int r=rec(root->right,x,y);
+    if(l!=-1 && r!=-1)
+    return root->data;
+    if(l==-1)
+    return r;
+    return l;
+}
+int lowestCommonAncestor(TreeNode<int> *root, int x, int y){
+    return rec(root,x,y);
+}
+
 
