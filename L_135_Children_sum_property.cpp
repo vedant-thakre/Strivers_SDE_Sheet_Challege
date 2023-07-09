2be312d53fb03014596da951c868207d798fa09c
ved-thakre <vedantthakre7@gmail.com>
2023-07-09 22:19:13 +0530

    Day-36

---
+++ L_135_Children_sum_property.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h> 
+
+void changeTree(BinaryTreeNode < int > * root) {
+    if(root == NULL) return;
+    int child = 0;
+    if(root->left){
+        child += root->left->data;
+    }
+    if(root->right){
+
+       child += root->right->data;
+
+    }
+    if(child >= root->data){
+        root->data = child;
+    }
+    else{
+        if(root->left) root->left->data = root->data;
+        if(root->right) root->right->data = root->data;
+    }
+    changeTree(root->left); 
+    changeTree(root->right); 
+
+    // backtracking
+    int total = 0;
+    if(root->left){
+        total += root->left->data;
+    }
+    if(root->right){
+        total += root->right->data;
+    }
+    if(root->left || root->right){
+        root->data = total;
+    }
+}  
+
