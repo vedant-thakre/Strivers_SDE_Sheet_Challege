0347bab1a2eaef1805c7df04e33a2c036ffeffeb
ved-thakre <vedantthakre7@gmail.com>
2023-07-10 22:54:17 +0530

    Day-37

---
+++ L_141_LCA_of_two_nodes_in_BST.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h> 
+
+TreeNode<int>* LCAinaBST(TreeNode<int>* root, TreeNode<int>* P, TreeNode<int>* Q){ 
+
+  if(root==NULL)return NULL;
+
+    if(root->data>P->data&&root->data>Q->data)
+
+ return LCAinaBST(root->left,P,Q);   
+
+ else
+    if(root->data<P->data&&root->data<Q->data)
+   return LCAinaBST(root->right,P,Q);   
+
+   else{   
+     return root;  
+  }
+}  
