ebf5ae0c5f5f3e3720f6b31463c245ce536e7b75
ved-thakre <vedantthakre7@gmail.com>
2023-07-24 22:35:36 +0530

    Day-51

---
+++ L_179_Ways_to_make_coin_change.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+
+long countWaysToMakeChange(int *num, int n, int target)
+
+{
+
+    vector<vector<long>> dp(n,vector<long>(target+1,0));
+
+    for(int t=0;t<=target;t++){
+
+       dp[0][t]=(t%num[0]==0);
+
+     }
+
+    for(int i=1;i<n;i++){
+
+       for(int t=0;t<=target;t++){
+
+           long not_take=dp[i-1][t];
+
+           long take=0;
+
+           if(num[i]<=t){
+
+               take=dp[i][t-num[i]];
+
+           }
+
+           dp[i][t]=take+not_take;
+
+       }
+
+   }
+
+    return dp[n-1][target];
+
+}
